a56d54bf82d1cc23000c764c897ec33e450fe099
Vitor Gabriel Melo Silva <vitorextre@gmail.com>
2026-04-07 13:50:58 -0300

    Update testezada.py

--- testezada.py
+++ testezada.py
@@ -35,25 +35,25 @@ navegador.switch_to.window(abas[1]) # muda a aba que os proximos comandos serao 
 lista_vagas = navegador.find_elements("class name", "btn-primary")
 
 lista_vagas[4].click() # clicka no item 5 dos elementos da classe btn-primary
-enviarCandidatura = navegador.find_element("class name", "btn-success") # testa mensagem de erro no formulario
-#enviarCandidatura.click() clickar aqui ele bloqueia
+enviarCandidatura = navegador.find_element("class name", "btn-success") 
+#enviarCandidatura.click() clickar aqui ele bloqueia e testa mensagem de erro no formulario
 
 time.sleep(1)
 
 #preenchendo candidatura
 navegador.find_element("id", "Candidato_Nome").send_keys("Vitaooo")
-navegador.find_element("id", "Candidato_Email").send_keys("vitorzada@gmail.com")      # escreve no campo com o id passado para o parametro
+navegador.find_element("id", "Candidato_Email").send_keys("vitorzada@gmail.com") # escreve no campo com o id passado para o parametro
 navegador.find_element("id", "Candidato_Celular").send_keys("11999999999")
 botaoCV =navegador.find_element("id", "cv-file")
 botaoCV.send_keys(r"C:\Users\Windows 10\Desktop\teste\CurriculoVitorGM.docx") # ja envia o arquivo direto!
 navegador.find_element("id", "Candidato_Mensagem").send_keys("Sou muito legal e estou aprendendo selenium (Isso é uma automação)")
 
 #Dar scroll na tela 
-navegador.execute_script('window.scrollBy(0,320)') # o segundo paramentro do scroll é o vertical negativo pra cima e positiva pra baixo
+navegador.execute_script('window.scrollBy(0,320)') # o segundo paramentro do scroll é o vertical, negativo pra cima e positiva pra baixo
 
-#tem um captcha que estou acionando manualmente
+#tem um captcha que estou acionando manualmente por isso dou 5 segundos para isso
 time.sleep(5)
 
-enviarCandidatura.click()
+enviarCandidatura.click() # enfim, envio a candidatura
 
 time.sleep(10)
